5d2da63a215562d3f857fe971a1331765e949e4c
Fahim Kashem <fkashem85@outlook.com>
2021-04-28 21:19:45 -0400

    'Create' functionality

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mealsRouter = require('./routes/meals');
-const FoodRecipes = require('./routes/foodrecipe');
 
 var app = express();
 
@@ -19,24 +18,5 @@ app.use(express.static(path.join(__dirname, '/public'), { extensions: 'html' }))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/meals', mealsRouter);
-
-// Create a user from the create-account form
-app.post("/create", function(req, res) {    
-    const user = new FoodRecipes({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        password: req.body.password
-    });
-
-    user.save(function(err, user) {
-        if (err) {
-            res.status(400).send(err);
-        }
-        else {
-            res.send("User is created");
-        }
-    });
-});
 
 module.exports = app;
--- public/scripts/loginScript.js
+++ public/scripts/loginScript.js
@@ -1,4 +1,39 @@
-/* https://github.com/RajnishKatharotiya/share-data-between-html-pages */
+var express = require('express');
+var app = express();
+const FoodRecipes = require('../routes/foodrecipe');
+
+var fname = document.getElementById("result-fname");
+var lname = document.getElementById("result-lname");
+var email = document.getElementById("result-email");
+var userInfo = document.getElementsByClassName("userInfo");
+
+// Create a user from the create-account form
+app.post("/create", function(req, res) {    
+    const user = new FoodRecipes({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save(function(err, user) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else {
+            res.send(`${req.body.fname} ${req.body.lname}'s account is created.\n
+                    Please wait for the website to redirect you.`);
+            setTimeout(function() { window.open('../pages/profile.html', '_self', false); }, 3000);
+            fname.value = req.body.fname;
+            lname.value = req.body.lname;
+            email.value = req.body.email;
+            userInfo.style.display = block;
+        }
+    });
+});
+
+module.exports = app;
+
 /*function loginValues() {
     console.log("loginValues() function");
 
