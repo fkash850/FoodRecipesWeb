61fec66f3c2443ff11b836d7d900bb1a1b624997
Fahim Kashem <fkashem85@outlook.com>
2021-04-28 22:23:25 -0400

    'Read' operation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mealsRouter = require('./routes/meals');
+const FoodRecipes = require('../routes/foodrecipe');
 
 var app = express();
 
@@ -18,5 +19,44 @@ app.use(express.static(path.join(__dirname, '/public'), { extensions: 'html' }))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/meals', mealsRouter);
+
+// Create a user from the create-account form
+app.post("/create", function(req, res) {    
+    const user = new FoodRecipes({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save(function(err, user) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        else {
+            res.send(user.fname + " " + user.lname + "'s account is created.\n"
+                    + "Please wait for the website to redirect you.");
+            setTimeout(function() { window.open('./public/pages/profile.html', '_self', false); }, 3000);
+        }
+    });
+});
+
+// Get user from the sign-in form
+app.get("/read", function(req, res) {
+    const user = new FoodRecipes({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    FoodRecipes.findOne({ email: new RegExp(user.email, "i") }, 
+        function(err, user) {
+            if (user === null) {
+                res.status(400).send(err);
+            }
+            else {
+                res.send(user.fname + " " + user.lname + "\n" + user.email);
+            }
+    });
+});
 
 module.exports = app;
